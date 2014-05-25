0af019be14ad750bf934748417dd49b8d69e87a7
Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>
2014-05-26 07:07:39 +0800

    refactor some test
    Signed-off-by: Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>

--- test/lib/contentTest.js
+++ test/lib/contentTest.js
@@ -3,14 +3,22 @@ var should = require('should'),
 
 describe('Content', function() {
     describe('load', function() {
-        it('should return false when file not found', function() {
-            content.load('pages', 'FOO').should.be.false;
-            content.load('posts', 'FOO').should.be.false;
-            content.load('foobar', 'FOO').should.be.false;
-        });
         it('should return array when file is found', function() {
-            content.load('pages', 'about').should.be.instanceOf(Array);
-            content.load('posts', 'orchestra/hello-world').should.be.instanceOf(Array);
-        });
-    });
+            content.load('pages', 'about').should.be.an.Array.and.have.lengthOf(2);
+            content.load('posts', 'orchestra/hello-world').should.be.an.Array.and.have.lengthOf(2);
+        })
+
+        it('should return false when file not found', function() {
+            content.load('foo', 'foo-bar').should.be.false;
+        })
+    })
+
+    describe('getMetadata', function() {
+        it('should return object when parsing metadata', function() {
+            var stub = content.load('pages', 'about');
+
+            stub.should.be.an.Array.and.have.lengthOf(2);
+            content.getMetadata(stub[0]).should.be.an.Object;
+        })
+    })
 });
